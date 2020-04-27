23dabc0849f02157329f32a85160ca9bfde269ad
Md Jahed <jahed.w.dev@gmail.com>
2020-04-27 16:35:51 +0600

    Milestone-1 (Day-1 Task-2)

---
+++ Milestone-1/Day-1/Task-2.js
@@ -0,0 +1,22 @@
+// TODO: Task-2 (javascript Number)
+// isNaN, parseFloat, parseInt;
+
+// 01 - isNaN();
+// The Number.isNaN() method determines whether a value is NaN (Not-A-Number). this is return true or false
+//Example:
+//Number.isNaN(NaN);        // true
+//Number.isNaN('NaN');      // false
+//Number.isNaN(37);         // false
+
+//02 - parseFloat();
+//The parseFloat() function parses a string and returns a floating point number.
+//Example:
+//const parsed = Number.parseInt('333fafa1545');
+//console.log(parsed)                               333
+//console.log(Number.parseFloat('13132.5fafa'))     13132.5
+
+//03 - parseInt();
+//The parseInt() function parses a string and returns an integer.
+//Example:
+// const parsed = Number.parseInt('33.3fafa1545');
+// console.log(parsed)                      33
